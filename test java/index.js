f806049b16b6a6da3512a37d5b1b4b9167611a11
hagermohamedshaban-cpu <hager.mohamed.shaban@gmail.com>
2026-07-21 05:36:52 +0300

    test java

---
+++ test java/index.js
@@ -0,0 +1,85 @@
+// for(var i=0;i<5;i++){
+//     console.log('cont &{i}');
+// }
+// var a=1;
+// while(a<5){
+//     console.log(`cont 2 &{i}`);
+//     a++;
+// }
+// var y="";
+// var x=1;
+// do{
+// y+=x;
+// }while (x<5);
+// console.log(y);
+
+// var user = {
+//     fullname:"hager mohamed",
+//     age:23,
+//     gender:"female",
+//     job:"Developer",
+// eat:function (female) {
+//     console.log(`hager is eating ${female}`
+
+//     )
+    
+    
+// }
+
+// }
+
+// console.log(user.fullname,user.gender,user.eat)
+
+
+//function parm
+
+// function calcwatch(wach,profil,tex,ads){
+//     var sum =wach+profil+tex+ads;
+// console.log(sum);
+
+// }
+// calcwatch()
+// calcwatch(100, 50, 30, 20);
+
+// scpo function
+// function scopchek(){
+//     var y=10;
+
+// }
+// scopchek();
+// glopal function
+// var x=30;
+// console.log(x);
+
+// expration function
+// function sum(num1, num2){
+//     console.log(num1 + num2);
+// }
+
+// sum(10, 20); 
+
+// arow function
+// const sum = (num1, num2) => console.log(num1 + num2);
+
+// sum(10, 20); 
+
+(function(){
+    function declare(){
+        console.log(`hello hager`);
+        
+    }
+    declare()
+
+
+})()
+
+
+
+
+
+
+
+
+
+
+
